b62c7faefed64ff81ddeb37e064995cabb035e7d
ronniemh <ronnieduardo.mh@gmail.com>
2019-07-03 16:25:20 -0500

    Desarrollo del backend

---
+++ src/main/java/com/woflion/app/servidorrest/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.woflion.app.servidorrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}

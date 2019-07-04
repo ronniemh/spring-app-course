2065bb7917990104b69d27b86d968c263aacb1a7
ronniemh <ronnieduardo.mh@gmail.com>
2019-07-04 11:49:10 -0500

    Rest controller get all

--- src/main/java/com/woflion/app/servidorrest/controllers/ClienteRestController.java
+++ src/main/java/com/woflion/app/servidorrest/controllers/ClienteRestController.java
@@ -6,10 +6,12 @@ import com.woflion.app.servidorrest.models.entity.Cliente;
 import com.woflion.app.servidorrest.models.services.IClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController{
